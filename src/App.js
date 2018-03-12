c54e445b5075bff6e6af227ba4bcab8d0003bb15
mschaeffner <1931755+mschaeffner@users.noreply.github.com>
2018-03-13 09:51:50 +1300

    style simple email ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,104 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default () =>
-  <div>
-    some text
-  </div>
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  background: #DDD;
+`
+
+const Header = styled.div`
+`
+
+const Content = styled.div`
+  flex: 1;
+  box-sizing: border-box;
+  padding: 20px;
+  padding-top: 0;
+`
+
+const Text = styled.textarea`
+  flex: 1;
+  resize: none;
+  border: 0;
+  background: white;
+  height: 100%;
+  width: 100%;
+  outline; none;
+  border-radius: 5px;
+  padding: 10px;
+  line-height: 1.2em;
+  box-sizing: border-box;
+  font-family: Verdana;
+  font-size: 24px;
+
+`
+
+const Line = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 20px;
+`
+
+const LineLabel = styled.div`
+  width: 80px;
+  font-weight: bold;
+  padding: 5px;
+`
+
+const LineValue = styled.div`
+  flex: 1;
+`
+
+const Name = styled.span`
+  background: white;
+  border-radius: 20px;
+  padding: 5px 15px;
+  display: inline-block;
+`
+
+
+export default ({from, to, subject, text}) =>
+  <Container>
+
+    <Header>
+
+      <Line>
+        <LineLabel>
+          Von:
+        </LineLabel>
+        <LineValue>
+          <Name>
+            {to}
+          </Name>
+        </LineValue>
+      </Line>
+        
+      <Line>
+        <LineLabel>
+          An:
+        </LineLabel>
+        <LineValue>
+          <Name>
+            {to}
+          </Name>
+        </LineValue>
+      </Line>
+
+    </Header>
+    
+    <Content>
+      <Text>
+        dvdvsdvds vds v ds vds v ds vdsv
+        dvdvsdvds vds v ds vds v ds vdsv
+        dvdvsdvds vds v ds vds v ds vdsv
+        dvdvsdvds vds v ds vds v ds vdsv
+        dvdvsdvds vds v ds vds v ds vdsv
+        dvdvsdvds vds v ds vds v ds vdsv
+        dvdvsdvds vds v ds vds v ds vdsv
+      </Text>
+    </Content>
+
+  </Container>
--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App
+    from='Mathias Schaeffner'
+    to='Max Mustermann'
+    subject='Neue Verordnung über den Datenschutz'
+    text=''
+  />, document.getElementById('root'));
